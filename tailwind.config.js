df679804587799833e939d315246eb1dd4928622
ruanvalente <ruanopensuselinux@gmail.com>
2022-05-22 20:00:33 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#8257e5"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
